d34a10cbd3fcc478aa10d59dd5c6fb5489b7bf20
Brenton Durkee <brentdur@gmail.com>
2015-04-25 17:49:23 -0400

    Mixed templates

---
+++ templates/js/app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular.module('resignApp', [
+  'ui.router',
+  'ui.bootstrap'
+])
+  .factory('people', function ($http) {
+      var ret = {
+        people: []
+      };
+
+
+      ret.getPeople = function(){
+        $http.get('people.json').success(function(data){
+          console.log("hello");
+          for(var i = 0; i <data.length; i++){
+            ret.people.push(data[i]);
+          }
+          console.log(ret.people);
+          return;
+        });
+      }
+
+      return ret;
+  })
+
+  .controller('mainCtrl', function ($scope, people, $timeout) {
+    people.getPeople();
+    $scope.people = people.people;
+    $scope.alerts = [];
+
+    $scope.addAlert = function(type, msg) {
+      $scope.alerts.push({type: type, msg: msg});
+      $scope.removeAlert($scope.alerts.length-1);
+    };
+
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
+    };
+
+    $scope.removeAlert = function(index){
+      $timeout(function(){
+        $scope.closeAlert(index);
+      }, 2000);
+    };
+    
+
+    $scope.submit = function(){
+      $scope.addAlert('success', 'Successfully checked in!');
+      $scope.name = '';
+      $scope.email = '';
+    };
+  $scope.select = function(item){
+      $scope.name = item.name;
+      $scope.email = item.contact.email;
+    };
+    
+  });
